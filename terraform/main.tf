978fbb32a6db120e29ccd22ee5cc46139579a97c
[Albert Boakye] <[albertbky@gmail.com]>
2023-09-06 04:24:25 -0400

    add new file

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+
+
+
+export KOPS_STATE_STORE= "fusion1-state-tf"
+export NODE_SIZE= "m5.large"
+export ZONES= "us-east-1a,us-east-1b,us-east-1c"
+kops create cluster kds.com \
+--node-count 3 \
+--zones $zones \
+--node-size $NODE_SIZE \
+--control-plane-size $CONTROL_PLANE_SIZE \
+--control-plane-size $ZONES
+--networking-cilium \
+--topology private \
+--cloud=aws \
+--bastian="true"
+--out=./terraform.main.tf \
+--target=terraform
